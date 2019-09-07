6cc2b70b26de11666396e786846455576861b1d0
Lucas Tostes <lucasrmtostes@gmail.com>
2019-09-07 19:58:13 -0300

    Update serve.py
    Updated color order

--- serve.py
+++ serve.py
@@ -149,8 +149,10 @@ def hello():
 @app.route("/player/<int:player_number>")
 def player(player_number):
 
-    player_background_colors    = ['#eb1f10', '#1732ff', '#098a00', '#ffa600', '#8749b3',' #ff4d4d', '#ff4400', '#452302', '#adadad', '#000000', '#ffffff']
+    player_background_colors    = ['#8749b3',' #ff4d4d', '#ffa600', '#ff4400', '#1732ff', '#eb1f10', '#452302', '#098a00', '#adadad', '#000000', '#ffffff']
     player_text_colors          = ['#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#ffffff', '#000000']
+
+    # Purple, Pink, Yellow, Orange, Blue, Red, Brown, Green, Gray, Black, White
 
     number_of_players, start_time, round = get_stats()
 
